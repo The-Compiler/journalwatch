c75f1e62e35d9ed7d682945c2365be1c24c28bbf
Florian Bruhin <git@the-compiler.org>
2016-11-30 21:38:18 +0100

    Don't import package in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
+import io
+import os.path
+import re
 from distutils.core import setup
 
-import journalwatch
+
+def read(*names, **kwargs):
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *names),
+        encoding=kwargs.get("encoding", "utf8")
+    ) as fp:
+        return fp.read()
 
 
-with open('README.asciidoc') as f:
-    description = f.read()
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
 
 
 setup(
@@ -14,11 +28,11 @@ setup(
     url='http://git.the-compiler.org/journalwatch/',
     author="Florian Bruhin",
     author_email='me@the-compiler.org',
-    version=journalwatch.__version__,
+    version=find_version('journalwatch.py'),
     py_modules=['journalwatch'],
     scripts=['journalwatch'],
     license='GPL',
-    long_description=description,
+    long_description=read('README.asciidoc'),
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
